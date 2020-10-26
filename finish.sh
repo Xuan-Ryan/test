ffc47bced5a0bc76fddefd734d1413dabfdfb194
tiger <tiger@mct.com.tw>
2020-10-26 06:42:23 +0000

    Add tools for SVN.
    git-svn-id: svn://192.168.1.250/UVC@8 a8a654e3-9019-44d2-b89c-6867c96d4060

---
+++ finish.sh
@@ -0,0 +1,17 @@
+# un-tart this file
+tar xvjf MT7620-MT7612.tar.bz2
+
+# revert all files from SVN
+svn revert -R RT288x_SDK/Uboot/*
+svn revert    RT288x_SDK/source/Makefile
+svn revert -R RT288x_SDK/source/final/*
+svn revert -R RT288x_SDK/source/mctconfig/*
+svn revert -R RT288x_SDK/source/vendors/*
+svn revert    RT288x_SDK/source/user/Makefile
+svn revert -R RT288x_SDK/source/user/lighttpd-1.4.20/*
+svn revert -R RT288x_SDK/source/user/rt2880_app/nvram/*
+svn revert -R RT288x_SDK/source/user/rt2880_app/scripts/*
+# if we add more files in source tree we need to add here too.
+
+# remove the archive file, which we don't need it.
+rm MT7620-MT7612.tar.bz2
