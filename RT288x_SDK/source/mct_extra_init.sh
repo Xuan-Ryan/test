08cc603c76c0b06ce9b5405414ea322344be5827
tiger <tiger@mct.com.tw>
2021-02-25 05:45:19 +0000

    Now we can choose the role by 'make' command.
    git-svn-id: svn://192.168.1.250/UVC@57 a8a654e3-9019-44d2-b89c-6867c96d4060

---
+++ RT288x_SDK/source/mct_extra_init.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+if [ ! -e .type ]; then
+	echo "Hello, You didn't select RX or TX yet, please make your choice."
+	read -p "Compile it as RX(Y/n):" ans
+	ans=`echo $ans|tr '[:lower:]' '[:upper:]'`
+	if [ -z $ans ]; then
+		echo 'You choice to compile as RX'
+		echo "RX" > .type
+	elif [ $ans = 'YES' -o $ans = 'Y' ]; then
+		echo 'You choice to compile as RX'
+		echo "RX" > .type
+	else
+		echo 'You choice to compile as TX'
+		echo "TX" > .type
+	fi
+	echo "******************************************"
+	echo "Please execute 'make' again"
+	echo "******************************************"
+	exit 1
+fi
+
+__TYPE=`cat .type`
+
+make oldconfig_config
+if [ $__TYPE = 'RX' ]; then
+	cp mctconfig/linux-config linux-2.6.36.x/.config
+	cp mctconfig/userconfig config/.config
+	cp mctconfig/busybox-config user/busybox/.config
+	cp mctconfig/mct-config.in config/config.in 
+	cp mctconfig/uClibc-config uClibc-0.9.33.2/.config
+else
+	cp mctconfig/linux-config-tx linux-2.6.36.x/.config
+	cp mctconfig/userconfig-tx config/.config
+	cp mctconfig/busybox-config user/busybox/.config
+	cp mctconfig/mct-config.in config/config.in 
+	cp mctconfig/uClibc-config uClibc-0.9.33.2/.config
+fi
+
+./mctconfig/patch/do-patch.sh
