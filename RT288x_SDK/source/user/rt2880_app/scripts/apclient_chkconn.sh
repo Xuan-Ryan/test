4f6707d8335570de4af99072a4bec2bfe5e0741e
tiger <tiger@mct.com.tw>
2021-02-25 12:08:34 +0000

    Stop doing check when we press WPS button.
    git-svn-id: svn://192.168.1.250/UVC@69 a8a654e3-9019-44d2-b89c-6867c96d4060

--- RT288x_SDK/source/user/rt2880_app/scripts/apclient_chkconn.sh
+++ RT288x_SDK/source/user/rt2880_app/scripts/apclient_chkconn.sh
@@ -19,27 +19,29 @@ do
 
 	sleep $interval
 
-	if [ "$opmode" != "1" ]; then
-		# check connection via conn_status command
-		disconnect=`iwpriv apclii0 conn_status|grep ApClii0|grep Disconnect`
-		if [ -n "$disconnect" ]; then
-			#  disconnect, do connection again
-			ssid=`nvram_get rtdev ApCliSsid`
-			if [ -n "$ssid" ]; then
-				ifconfig apclii0 down up
-				iwpriv apclii0 set ApCliEnable=1
-				iwpriv apclii0 set SiteSurvey=1
-				sleep 1
-				iwpriv apclii0 get_site_survey
-				iwpriv apclii0 set ApCliAutoConnect=1
-			fi
-		else
-			count=0
-			#  store the SSID from AP
-			#  ex: ApClii0 Connected AP : 00:05:1B:00:01:02   SSID:UVC-79366D
-			SSID=`iwpriv apclii0 conn_status|awk -F ' ' '{print $6}'|awk -F ':' '{print $2}'`
-			if [ -n "$SSID" ]; then
-				nvram_set rtdev ApCliSsid $SSID
+	if [ ! -e /tmp/doing_wps ]; then
+		if [ "$opmode" != "1" ]; then
+			# check connection via conn_status command
+			disconnect=`iwpriv apclii0 conn_status|grep ApClii0|grep Disconnect`
+			if [ -n "$disconnect" ]; then
+				#  disconnect, do connection again
+				ssid=`nvram_get rtdev ApCliSsid`
+				if [ -n "$ssid" ]; then
+					ifconfig apclii0 down up
+					iwpriv apclii0 set ApCliEnable=1
+					iwpriv apclii0 set SiteSurvey=1
+					sleep 1
+					iwpriv apclii0 get_site_survey
+					iwpriv apclii0 set ApCliAutoConnect=1
+				fi
+			else
+				count=0
+				#  store the SSID from AP
+				#  ex: ApClii0 Connected AP : 00:05:1B:00:01:02   SSID:UVC-79366D
+				SSID=`iwpriv apclii0 conn_status|awk -F ' ' '{print $6}'|awk -F ':' '{print $2}'`
+				if [ -n "$SSID" ]; then
+					nvram_set rtdev ApCliSsid $SSID
+				fi
 			fi
 		fi
 	fi
