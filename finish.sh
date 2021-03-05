13691ea42de49fba372059adf8fa8ca32ec0ed06
tiger <tiger@mct.com.tw>
2021-03-05 06:34:08 +0000

    We need to do the 'revert' for new download.
    git-svn-id: svn://192.168.1.250/UVC@76 a8a654e3-9019-44d2-b89c-6867c96d4060

--- finish.sh
+++ finish.sh
@@ -2,6 +2,7 @@
 tar xvjf MT7620-MT7612.tar.bz2
 
 # revert all files from SVN
+svn revert -R RT288x_SDK/source/*
 svn revert -R RT288x_SDK/Uboot/*
 svn revert    RT288x_SDK/source/Makefile
 svn revert -R RT288x_SDK/source/final/*
@@ -11,6 +12,7 @@ svn revert    RT288x_SDK/source/user/Makefile
 svn revert -R RT288x_SDK/source/user/lighttpd-1.4.20/*
 svn revert -R RT288x_SDK/source/user/rt2880_app/nvram/*
 svn revert -R RT288x_SDK/source/user/rt2880_app/scripts/*
+svn revert -R RT288x_SDK/source/user/alsa-lib-1.0.27.2/*
 # if we add more files in source tree we need to add here too.
 
 # remove the archive file, which we don't need it.
