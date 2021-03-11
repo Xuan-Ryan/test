a7c413c863d352e2ebcffcfa1c8bfd6c332a20bd
tiger <tiger@mct.com.tw>
2021-03-11 10:59:44 +0000

    1. Add LED control. 2. DO NOT write the wpapsk1 key which gets from 'iwpriv apcli0 stat'.
    git-svn-id: svn://192.168.1.250/UVC@102 a8a654e3-9019-44d2-b89c-6867c96d4060

--- RT288x_SDK/source/user/rt2880_app/scripts/apclient_chkconn.sh
+++ RT288x_SDK/source/user/rt2880_app/scripts/apclient_chkconn.sh
@@ -8,41 +8,114 @@ nm=`nvram_get 2860 lan_netmask`
 interval=10
 
 rm /tmp/timeout 1>/dev/null 2>/dev/null
+opmode=`nvram_get 2860 OperationMode`
+if [ "$opmode" = "1" ]; then
+	exit
+fi
+
+connected="0"
+disconnect=`iwpriv apclii0 conn_status|grep ApClii0|grep Disconnect`
+if [ -n "$disconnect" ]; then
+	# turn off green LED
+	gpio l 52 0 4000 0 1 4000
+	# light up red LED
+	gpio l 14 4000 0 1 0 4000
+else
+	connected="1"
+	# light up green LED
+	gpio l 52 4000 0 1 0 4000
+	# light up red LED
+	gpio l 14 4000 0 1 0 4000
+fi
 
 while :
 do
-	opmode=`nvram_get 2860 OperationMode`
-	if [ "$opmode" = "1" ]; then
-		sleep 5
-		continue
-	fi
-
 	sleep $interval
 
 	if [ ! -e /tmp/doing_wps ]; then
-		if [ "$opmode" != "1" ]; then
-			# check connection via conn_status command
-			disconnect=`iwpriv apclii0 conn_status|grep ApClii0|grep Disconnect`
-			if [ -n "$disconnect" ]; then
-				#  disconnect, do connection again
-				ssid=`nvram_get rtdev ApCliSsid`
-				if [ -n "$ssid" ]; then
-					ifconfig apclii0 down up
-					iwpriv apclii0 set ApCliEnable=1
-					iwpriv apclii0 set SiteSurvey=1
-					sleep 1
-					iwpriv apclii0 get_site_survey
-					iwpriv apclii0 set ApCliAutoConnect=1
-				fi
-			else
-				count=0
-				#  store the SSID from AP
-				#  ex: ApClii0 Connected AP : 00:05:1B:00:01:02   SSID:UVC-79366D
-				SSID=`iwpriv apclii0 conn_status|awk -F ' ' '{print $6}'|awk -F ':' '{print $2}'`
-				if [ -n "$SSID" ]; then
-					nvram_set rtdev ApCliSsid $SSID
+		interval=10
+		# check connection via conn_status command
+		disconnect=`iwpriv apclii0 conn_status|grep ApClii0|grep Disconnect`
+		if [ -n "$disconnect" ]; then
+			if [ "$connected" = "1" ]; then
+				connected="0"
+				# turn off green LED
+				gpio l 52 0 4000 0 1 4000
+				# light up red LED
+				gpio l 14 4000 0 1 0 4000
+			fi
+			#  disconnect, do connection again
+			ssid=`nvram_get rtdev ApCliSsid`
+			if [ -n "$ssid" ]; then
+				ifconfig apclii0 down up
+				sleep 2
+				iwpriv apclii0 set ApCliEnable=1
+				iwpriv apclii0 set SiteSurvey=1
+				sleep 1
+				iwpriv apclii0 get_site_survey
+				iwpriv apclii0 set ApCliAutoConnect=1
+			fi
+		else
+			count=0
+			#  store the SSID from AP
+			#  ex: ApClii0 Connected AP : 00:05:1B:00:01:02   SSID:UVC-79366D
+			CUR_SSID=`iwpriv apclii0 conn_status|awk -F ' ' '{print $6}'|awk -F ':' '{print $2}'`
+			if [ -n "$CUR_SSID" ]; then
+				ORG_SSID=`nvram_get rtdev ApCliSsid`
+				SSID=`iwpriv apclii0 stat|grep SSID`
+				SSID=`echo $SSID|awk -F '=' '{print $2}'`
+				SSID=`echo $SSID|xargs`
+				AUTHTYPE=`iwpriv apclii0 stat|grep AuthType`
+				AUTHTYPE=`echo $AUTHTYPE|awk -F '=' '{print $2}'`
+				AUTHTYPE=`echo $AUTHTYPE|xargs`
+				ENCRYPTYPE=`iwpriv apclii0 stat|grep EncrypType`
+				ENCRYPTYPE=`echo $ENCRYPTYPE|awk -F '=' '{print $2}'`
+				ENCRYPTYPE=`echo $ENCRYPTYPE|xargs`
+				KEY=`iwpriv apclii0 stat|grep "Key "`
+				KEY=`echo $KEY|awk -F '=' '{print $2}'`
+				KEY=`echo $KEY|xargs`
+				if [ -n "$SSID" ] && [ "$SSID" != "$ORG_SSID" ]; then
+					nvram_set rtdev ApCliSsid "$SSID"
+
+					if [ -n `echo $AUTHTYPE|grep WPA2` ]; then
+						nvram_set rtdev ApCliAuthMode WPA2PSK
+						#nvram_set rtdev ApCliWPAPSK "$KEY"
+					elif [ -n `echo $AUTHTYPE|grep WPA` ]; then
+						nvram_set rtdev ApCliAuthMode WPAPSK
+						#nvram_set rtdev ApCliWPAPSK "$KEY"
+					elif [ -n `echo $AUTHTYPE|grep SHARED` ]; then
+						nvram_set rtdev ApCliAuthMode SHARED
+						#nvram_set rtdev ApCliKey1Str "$KEY"
+					elif [ -n `echo $AUTHTYPE|grep OPEN` ]; then
+						nvram_set rtdev ApCliAuthMode OPEN
+						#nvram_set rtdev ApCliKey1Str "$KEY"
+					else
+						nvram_set rtdev ApCliAuthMode WPA2PSK
+						#nvram_set rtdev ApCliWPAPSK "$KEY"
+					fi
+
+					if [ -n `echo $ENCRYPTYPE|grep AES` ]; then
+						nvram_set rtdev ApCliEncrypType AES
+					elif [ -n `echo $ENCRYPTYPE|grep TKIP` ]; then
+						nvram_set rtdev ApCliEncrypType AES
+					elif [ -n `echo $ENCRYPTYPE|grep WEP` ]; then
+						nvram_set rtdev ApCliEncrypType WEP
+					elif [ -n `echo $ENCRYPTYPE|grep NONE` ]; then
+						nvram_set rtdev ApCliEncrypType NONE
+					else
+						nvram_set rtdev ApCliEncrypType AES
+					fi
 				fi
 			fi
+			if [ "$connected" = "0" ]; then
+				connected="1"
+				# light up green LED
+				gpio l 52 4000 0 1 0 4000
+				# light up red LED
+				gpio l 14 4000 0 1 0 4000
+			fi
 		fi
+	else
+		interval=10
 	fi
 done
