7fa63166c8de330b26d58560fcf1da57fb4efd78
tiger <tiger@mct.com.tw>
2021-04-19 10:27:25 +0000

    We create the configuration of the DHCP server in lan.sh takes too much time. So I modify the method of creating the configuration.
    git-svn-id: svn://192.168.1.250/UVC@188 a8a654e3-9019-44d2-b89c-6867c96d4060

--- RT288x_SDK/source/user/rt2880_app/scripts/lan.sh
+++ RT288x_SDK/source/user/rt2880_app/scripts/lan.sh
@@ -37,6 +37,11 @@ echo "$ip $host.j5create.com $host" >> /etc/hosts
 # dhcp server
 dhcp=`nvram_get 2860 dhcpEnabled`
 if [ "$dhcp" = "1" ]; then
+	fname="/etc/udhcpd.conf"
+	leases="/var/udhcpd.leases"
+	killall -q udhcpd
+	touch $leases
+
 	start=`nvram_get 2860 dhcpStart`
 	end=`nvram_get 2860 dhcpEnd`
 	mask=`nvram_get 2860 dhcpMask`
@@ -44,94 +49,15 @@ if [ "$dhcp" = "1" ]; then
 	sd=`nvram_get 2860 dhcpSecDns`
 	gw=`nvram_get 2860 dhcpGateway`
 	lease=`nvram_get 2860 dhcpLease`
-	static1=`nvram_get 2860 dhcpStatic1 | sed -e 's/;/ /'`
-	static2=`nvram_get 2860 dhcpStatic2 | sed -e 's/;/ /'`
-	static3=`nvram_get 2860 dhcpStatic3 | sed -e 's/;/ /'`
-
-	config-udhcpd.sh -s $start
-	config-udhcpd.sh -e $end
-	config-udhcpd.sh -i $lan_if
-	config-udhcpd.sh -m $mask
-	if [ "$pd" != "" -o "$sd" != "" ]; then
-		config-udhcpd.sh -d $pd $sd
-	fi
-	if [ "$gw" != "" ]; then
-		config-udhcpd.sh -g $gw
-	fi
-	if [ "$lease" != "" ]; then
-		config-udhcpd.sh -t $lease
-	fi
-	config-udhcpd.sh -S
-	if [ "$static1" != "" ]; then
-		config-udhcpd.sh -S $static1
-	fi
-	if [ "$static2" != "" ]; then
-		config-udhcpd.sh -S $static2
-	fi
-	if [ "$static3" != "" ]; then
-		config-udhcpd.sh -S $static3
-	fi
-
-	# Deal with "super dmz".
-	# udhcp has to lease the WAN ip/netmask/router settings to the 
-	# "super dmz" host on LAN.
-	dmz=`nvram_get 2860 DMZEnable`
-	dmzaddress=`nvram_get 2860 DMZAddress`
-	if [ "$dmz" = "2" -a "$dmzaddress" != "" -a "$opmode" != "0" ]; then
-		# Super dmz enabled.
-		# Get WAN IP/Netmask/Gateway
-		wip=`ifconfig $wan_ppp_if | sed -n '/inet addr:/p' | sed 's/ *inet addr:\([0-9\.]*\)\ \ .*/\1/'`
-		wnm=`ifconfig $wan_ppp_if | sed -n '/inet addr:/p' | sed 's/.*Mask:\([0-9\.]*\)$/\1/'`
-		wgw=`route -n | grep "^0.0.0.0" | sed  's/[0-9.]*//' | sed 's/^[ ]*//' | sed 's/\([0-9.]*\)[ ]*[a-zA-Z0-9 .]*/\1/'`
-
-		if [ "$wip" = "" -o "$wnm" = "" -o "$wgw" = "" ]; then
-			echo "SuperDMZ: Can't get $wan_ppp_if ip/netmask/gateway currently."
-		else
-			config-udhcpd.sh -S $dmzaddress $wip
-			config-udhcpd.sh -x $wnm
-			config-udhcpd.sh -y $wgw
-		fi
-	fi
-	config-udhcpd.sh -r 1
-fi
-
-
-# lltd
-lltd=`nvram_get 2860 lltdEnabled`
-if [ "$lltd" = "1" ]; then
-	lld2d $lan_if
-fi
-
-# igmpproxy
-igmp=`nvram_get 2860 igmpEnabled`
-if [ "$igmp" = "1" ]; then
-	config-igmpproxy.sh $wan_if $lan_if
-fi
-
-# upnp
-if [ "$opmode" != "0" ]; then
-	upnp=`nvram_get 2860 upnpEnabled`
-	if [ "$upnp" = "1" ]; then
-		if [ "$CONFIG_USER_MINIUPNPD" = "y" ]; then
-			miniupnpd.sh init
-		else
-			route add -net 239.0.0.0 netmask 255.0.0.0 dev $lan_if
-			upnp_xml.sh $ip
-			upnpd -f $wan_ppp_if $lan_if &
-		fi
-	fi
-fi
-
-
-# pppoe-relay
-pppr=`nvram_get 2860 pppoeREnabled`
-if [ "$pppr" = "1" ]; then
-	pppoe-relay -S $wan_if -B $lan_if
-fi
-
-# dns proxy
-dnsp=`nvram_get 2860 dnsPEnabled`
-if [ "$dnsp" = "1" ]; then
-	dnsmasq &
+	echo "start $start" > $fname
+	echo "end $end" >> $fname
+	echo "interface $lan_if" >> $fname
+	echo "option subnet $mask" >> $fname
+	echo "option dns $pd $sd" >> $fname
+	echo "option router $gw" >> $fname
+	echo "option lease $lease" >> $fname
+	echo "lease_file $leases" >> $fname
+	
+	udhcpd $fname
 fi
 
